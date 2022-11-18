c136a72c004724f544fc6a7ca409556c14deb6a8
JoseJacobe <jose.jacobe@ucalgary.ca>
2022-11-18 11:45:24 -0700

    Created new scene for after user has created their username and password and renamed some variables within the controller

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -23,17 +24,9 @@ public class LoginController {
 	 * @param event
 	 */
 	public void userLogin(ActionEvent event) {
-		
 
-		
 		Scene testScene = new Scene(new Label("Next Scene Label"), 600, 400);
 		applicationStage.setScene(testScene);
-		
-
-		
-
-		
-		
 		
 	}
 	
@@ -46,16 +39,43 @@ public class LoginController {
 	 */
 	public void registerUser(ActionEvent event) {
 		
-		
-		
 		try {
 			
 			FXMLLoader registerPage = new FXMLLoader();
 			VBox root = registerPage.load(new FileInputStream("src/application/RegisterView.fxml"));
 			
-			Scene testScene = new Scene(root, 600, 400);
-			applicationStage.setScene(testScene);
+			Scene registerScene = new Scene(root, 600, 400);
+			applicationStage.setScene(registerScene);
 			
+		}
+		
+		catch(Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+	}
+	
+	/**
+	 * 
+	 * Create new user's schedule after they have created their username and password
+	 * 
+	 * @param event
+	 */
+	public void registerSchedule(ActionEvent event) {
+		
+		
+		
+		try {
+			
+			FXMLLoader registerSchedulePage = new FXMLLoader();
+			VBox root = registerSchedulePage.load(new FileInputStream("src/application/CreateScheduleView.fxml"));
+			
+			Scene registerScheduleScene = new Scene(root, 600, 400);
+			
+			applicationStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			applicationStage.setScene(registerScheduleScene);
+			applicationStage.show();
 			
 			
 		}
@@ -68,12 +88,6 @@ public class LoginController {
 		}
 		
 
-
-		
-
-		
-		
-		
 	}
 	
 	
