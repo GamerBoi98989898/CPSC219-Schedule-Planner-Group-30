14b8c576145e62261eb53587db09469501437219
connor ell <connor.ell@ucalgary.ca>
2022-11-18 14:21:23 -0700

    More file reading implemented

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,19 +17,16 @@ public class Main {
 		test.setPassword("testpass");
 		test.saveToFile("src/test");
 		User.writeFile("src/testfile2.txt");
-		User.readFile("src/testfile.txt");*/
+		User.readFile("src/testfile.txt");
 		User test = new User("src/test");
 		test.setUsername("da man");
 		System.out.println(test.getUsername());
 		test.setPassword("da boi");
 		System.out.println(test.getPassword());
-		test.saveToFile("src/daFile");
-		
-	    long duration = 0;
-	    LocalTime start = LocalTime.MIN;
-	    LocalTime end = LocalTime.MAX;
-	    duration = start.until(end, ChronoUnit.HOURS);
-	    System.out.println(duration);
+		test.saveToFile("src/daFile");*/
+		User thing = new User("src/yeet.txt");
+		thing.getUsername();
+		thing.getPassword();
 	}
 
 }
--- src/application/User.java
+++ src/application/User.java
@@ -3,23 +3,111 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class User {
-	private String fileName; //May be removed
 	private String username;
 	private String password;
-	private ArrayList<String> timetable;
+	private ArrayList<String> suntimetable = new ArrayList<String>();
+	private ArrayList<String> montimetable = new ArrayList<String>();
+	private ArrayList<String> tuetimetable = new ArrayList<String>();
+	private ArrayList<String> wedtimetable = new ArrayList<String>();
+	private ArrayList<String> thutimetable = new ArrayList<String>();
+	private ArrayList<String> fritimetable = new ArrayList<String>();
+	private ArrayList<String> sattimetable = new ArrayList<String>();
 
 	public User() {
 	}
 	
 	//
 	public User(String fileName) {
+		
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(fileName));
 			this.username = reader.readLine();
 			this.password = reader.readLine();
 			System.out.println(username);
 			System.out.println(password);
-			reader.close();
+			
+			String line = reader.readLine();
+			while (line!=null) {
+				if (line.contains(":")) {
+					line = "time";
+				}
+				switch (line) {
+				case "sun":
+					System.out.println("sun was found");
+					line = reader.readLine();
+					while (line.contains(":")) {
+						System.out.println("writing times");
+						suntimetable.add(line);
+						line = reader.readLine();
+					}
+					continue;
+				case "mon":
+					System.out.println("mon was found");
+					line = reader.readLine();
+					while (line.contains(":")) {
+						System.out.println("writing times");
+						montimetable.add(line);
+						line = reader.readLine();
+					}
+					continue;
+				case "tue":
+					System.out.println("tue was found");
+					line = reader.readLine();
+					while (line.contains(":")) {
+						System.out.println("writing times");
+						tuetimetable.add(line);
+						line = reader.readLine();
+					}
+					continue;
+				case "wed":
+					System.out.println("wed was found");
+					line = reader.readLine();
+					while (line.contains(":")) {
+						System.out.println("writing times");
+						wedtimetable.add(line);
+						line = reader.readLine();
+					}
+					continue;
+				case "thu":
+					System.out.println("thu was found");
+					line = reader.readLine();
+					while (line.contains(":")) {
+						System.out.println("writing times");
+						thutimetable.add(line);
+						line = reader.readLine();
+					}
+					continue;
+				case "fri":
+					System.out.println("fri was found");
+					line = reader.readLine();
+					while (line.contains(":")) {
+						System.out.println("writing times");
+						fritimetable.add(line);
+						line = reader.readLine();
+					}
+					continue;
+				case "sat":
+					System.out.println("sat was found");
+					line = reader.readLine();
+					while (line.contains(":")) {
+						System.out.println("writing times");
+						sattimetable.add(line);
+						line = reader.readLine();
+					}
+					continue;
+				case "time":
+					System.out.println("time was found");
+					continue;
+				case "end":
+					System.out.println("eof was found");
+					break;
+				}
+				
+				
+				line = reader.readLine();
+			}
+			
+			reader.close();	
 		} catch (FileNotFoundException fnf) {
 			//
 			fnf.printStackTrace();
@@ -44,14 +132,6 @@ public class User {
 	
 	
 	// Getters and setters below
-	String getFileName() {
-		return fileName;
-	}
-
-	void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-
 	String getUsername() {
 		return username;
 	}
@@ -68,13 +148,60 @@ public class User {
 		this.password = password;
 	}
 
-	ArrayList<String> getTimetable() {
-		return timetable;
+	private ArrayList<String> getSuntimetable() {
+		return suntimetable;
 	}
 
-	void setTimetable(ArrayList<String> timetable) {
-		this.timetable = timetable;
+	private void setSuntimetable(ArrayList<String> suntimetable) {
+		this.suntimetable = suntimetable;
 	}
-	
+
+	private ArrayList<String> getMontimetable() {
+		return montimetable;
+	}
+
+	private void setMontimetable(ArrayList<String> montimetable) {
+		this.montimetable = montimetable;
+	}
+
+	private ArrayList<String> getTuetimetable() {
+		return tuetimetable;
+	}
+
+	private void setTuetimetable(ArrayList<String> tuetimetable) {
+		this.tuetimetable = tuetimetable;
+	}
+
+	private ArrayList<String> getWedtimetable() {
+		return wedtimetable;
+	}
+
+	private void setWedtimetable(ArrayList<String> wedtimetable) {
+		this.wedtimetable = wedtimetable;
+	}
+
+	private ArrayList<String> getThutimetable() {
+		return thutimetable;
+	}
+
+	private void setThutimetable(ArrayList<String> thutimetable) {
+		this.thutimetable = thutimetable;
+	}
+
+	private ArrayList<String> getFritimetable() {
+		return fritimetable;
+	}
+
+	private void setFritimetable(ArrayList<String> fritimetable) {
+		this.fritimetable = fritimetable;
+	}
+
+	private ArrayList<String> getSattimetable() {
+		return sattimetable;
+	}
+
+	private void setSattimetable(ArrayList<String> sattimetable) {
+		this.sattimetable = sattimetable;
+	}
 
 }
