4c511584f406cb36f553e7b527ec1cf1e265bcdc
JoseJacobe <jose.jacobe@ucalgary.ca>
2022-11-19 20:04:08 -0700

    Reupload correct AppController

--- src/application/AppController.java
+++ src/application/AppController.java
@@ -1,33 +1,46 @@
 package application;
 
-import java.io.FileInputStream;
 
+import java.io.IOException;
+
+
+import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class AppController {
+public class AppController extends Timeblock {
 	
 	Stage applicationStage;
 	
+	@FXML
+	Button loginButton;
 	
+	@FXML
+	Button registerButton;
+	
+	@FXML
+	Button completeRegisterButton;
 	
 	/**
 	 * 
 	 * Check if user already exists and if credentials are correct, proceed to show their current schedule
 	 * 
 	 * @param event
+	 * @throws IOException 
 	 */
-	public void userLogin(ActionEvent event) {
-
-		Scene testScene = new Scene(new Label("Next Scene Label"), 600, 400);
-		applicationStage.setScene(testScene);
+	public void userLogin(ActionEvent Event) throws Exception {
 		
+		
+			Parent root = FXMLLoader.load(getClass().getResource("ScheduleView.fxml"));
+			
+			Stage applicationStage = (Stage)loginButton.getScene().getWindow();
+			
+			applicationStage.setScene(new Scene(root, 600, 400));
+
 	}
 	
 	
@@ -36,60 +49,36 @@ public class AppController {
 	 * Register new user and prompt their username and password upon button press
 	 * 
 	 * @param event
+	 * @throws Exception 
 	 */
-	public void registerUser(ActionEvent event) {
+	public void registerUser(ActionEvent event) throws Exception {
 		
-		try {
-			
-			FXMLLoader registerPage = new FXMLLoader();
-			VBox root = registerPage.load(new FileInputStream("src/application/RegisterView.fxml"));
-			
-			Scene registerScene = new Scene(root, 600, 400);
-			applicationStage.setScene(registerScene);
-			
-		}
+		Parent root = FXMLLoader.load(getClass().getResource("RegisterView.fxml"));
 		
-		catch(Exception e) {
-			
-			e.printStackTrace();
-			
-		}
+		Stage applicationStage = (Stage)registerButton.getScene().getWindow();
+		
+		applicationStage.setScene(new Scene(root, 600, 400));
+		
 	}
+		
+	
 	
 	/**
 	 * 
-	 * Create new user's schedule after they have created their username and password
+	 * Once user has completed their registration, it will return to the main login screen for them to login
 	 * 
 	 * @param event
+	 * @throws Exception
 	 */
-	public void registerSchedule(ActionEvent event) {
+	public void completeRegister(ActionEvent event) throws Exception {
+		
+		Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+		
+		Stage applicationStage = (Stage)completeRegisterButton.getScene().getWindow();
+		
+		applicationStage.setScene(new Scene(root, 600, 400));
 		
 		
-		
-		try {
-			
-			FXMLLoader registerSchedulePage = new FXMLLoader();
-			VBox root = registerSchedulePage.load(new FileInputStream("src/application/CreateScheduleView.fxml"));
-			
-			Scene registerScheduleScene = new Scene(root, 600, 400);
-			
-			applicationStage = (Stage)((Node)event.getSource()).getScene().getWindow();
-			applicationStage.setScene(registerScheduleScene);
-			applicationStage.show();
-			
-			
-		}
-		
-		catch(Exception e) {
-			
-			e.printStackTrace();
-			
-			
-		}
-		
-
 	}
 	
-	
-
 }
